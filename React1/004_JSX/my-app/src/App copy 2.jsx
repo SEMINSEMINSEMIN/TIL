1f36b10e96175a323073b0f4f9194f21f9ae1319
강세민 <seminkang@gangsemin-ui-iMac.local>
2022-11-30 23:43:10 +0900

    221130

--- React1/004_JSX/my-app/src/App copy 2.jsx
+++ React1/004_JSX/my-app/src/App copy 2.jsx
@@ -1,23 +1,50 @@
 function App() {
-  const name = '라이캣!';
-  function 함수(){
+  const name = "라이캣!";
+  function 함수() {
     return 100;
   }
   const 변수 = 10;
   // const 값 = true;
   const 값 = false;
   const 값2 = null;
+
+  const sortedRender = (arr) => {
+    let str = "";
+    const sorted = arr.sort((a, b) => a - b);
+    for (let i in sorted) {
+      if (i === sorted.length - 1) {
+        str += `${sorted[i]}`;
+      } else {
+        str += `${sorted[i]}, `;
+      }
+    }
+    return str;
+  };
+
   return (
     <div className="App">
       <h1>안녕, {name} 1호</h1>
       <p>{100 + 1}</p>
-      <p>{'hello' + 'world'}</p>
-      <p>{[1, 2, 3].map(x => x ** 2)}</p>
+      <p>{"hello" + "world"}</p>
+      <p>{[1, 2, 3].map((x) => x ** 2)}</p>
       <p>{함수()}</p>
       <p>{변수}</p>
-      <p>{값 ? 'one': 'two'}</p>
+      <p>{값 ? "one" : "two"}</p>
       {/* nullish 연산자: 앞의 값이 null이거나 undefined라면 뒤의 값 */}
-      <p>{값2 ?? 'one'}</p>
+      <p>{값2 ?? "one"}</p>
+      <p
+        style={{
+          color: "blue",
+          fontSize: "30px",
+          backgroundColor: "yellow",
+          borderRadius: "3px",
+          border: "1px solid black",
+          padding: "10px",
+        }}
+      >
+        스타일 적용
+      </p>
+      <p>다음은 3, 1, 2, 5를 정렬한 결과입니다: {sortedRender([3, 1, 2, 5])}</p>
     </div>
   );
 }
---
+++ React1/README.md
@@ -0,0 +1,36 @@
+# 4. JSX
+
+## 4.1. 기초
+
+- JSX: JavaScript + XML
+
+## 4.2. JSX 문법
+
+### 4.2.1 최상위 부모요소는 1개
+
+### 4.2.2 주석
+
+### 4.2.3 태그에 클래스 넣기 + 스타일 적용하기
+
+- 클래스 정의: className
+- in-line 스타일 적용
+
+  ```html
+  <div style={{backgroundColor:"black", color:"white"}}></div>
+  ```
+
+  - 객체 형태로 정의
+  - camelCase(단어를 대문자로 구분): .css 파일을 만들어 사용할 때에는 그대로 사용
+
+### 4.2.4 자바스크립트 사용하기
+
+- 중괄호 내 JavaScript 문법 사용 가능
+  - App copy 2.jsx 참고
+
+## 4.3. 실습 과제
+
+## 참고
+
+- [JSX 좀 더 알아보기](https://paullabworkspace.notion.site/JSX-9a4ef8f7cecb406e8d2be2ff9383706f)
+  - props는 **리액트 컴포넌트를 사용하며 정의한 어트리뷰트들**이 모여있는 객체
+    ![](https://paullabworkspace.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F1e05956b-f5de-4fac-ad6e-5948beee9393%2FUntitled.png?table=block&id=85f8ed17-cbf2-42a6-84ad-edb4cc6e7a39&spaceId=579fe283-28aa-489d-ae65-d683304becfc&width=2000&userId=&cache=v2)
