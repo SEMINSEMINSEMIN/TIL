7742dee39597fcc18d29ce9b722bd721cc04b5bb
강세민 <ksm9802@gmail.com>
2022-12-02 11:47:15 +0900

    Pages 폴더 만들고 하위 페이지 넣은 후 App.js에서 불러오기

--- React1/쇼핑몰플젝/my-app/src/App.js
+++ React1/쇼핑몰플젝/my-app/src/App.js
@@ -6,13 +6,19 @@ import {
     useLocation,
 } from "react-router-dom";
 
+import Main from "./Pages/Main";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/products/:id" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/products/:id" element={<Product />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 export default App;
