ddc546ba94eb46a8cb4b4d25d20801378c64d2ed
SeminKang <ksm9802@gmail.com>
2022-12-02 14:57:04 +0900

    CartBtn 기능

--- React1/쇼핑몰플젝/my-app/src/Components/CartBtn.jsx
+++ React1/쇼핑몰플젝/my-app/src/Components/CartBtn.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CartBtn() {
+    const [bool, setBool] = useState(false);
+    const handleMouseEnter = () => setBool(true);
+    const handleMouseLeave = () => setBool(false);
     return (
-    <div>CartBtn</div>
+    <Link
+        to="/cart"
+        style={
+            bool === false ?
+            {border: "5px solid black", transition: "linear 0.5s"} :
+            {border: "5px solid red", transition: "linear 0.5s"}
+        }
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+    >
+        CartBtn
+    </Link>
     )
 }
         
