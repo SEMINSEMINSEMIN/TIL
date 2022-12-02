cd2c85db2d64abcb9f0e94784bfa173b2c5273bf
강세민 <ksm9802@gmail.com>
2022-12-02 12:05:22 +0900

    데이터 가져오기 작업중

--- React1/쇼핑몰플젝/my-app/src/App.js
+++ React1/쇼핑몰플젝/my-app/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
     BrowserRouter,
     Routes,
@@ -5,12 +6,17 @@ import {
     Outlet,
     useLocation,
 } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
 import Main from "./Pages/Main";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 
 function App() {
+  useEffect(() => {
+    axios.get("")
+  })
     return (
         <BrowserRouter>
             <Routes>
@@ -22,3 +28,28 @@ function App() {
     );
 }
 export default App;
+
+
+
+
+export default function Product() {
+  useEffect(() => {
+    // '마운트 될 때' '브라우저에 그려진 후' 딱 한 번 실행
+    axios.get("http://test.api.weniv.co.kr/mall").then((res) => {
+      console.log("axios");
+      console.log(res); // Object
+      console.log(res.data);
+    });
+    fetch("http://test.api.weniv.co.kr/mall")
+      .then((res) => {
+        console.log("fetch");
+        console.log(res); // Response
+        // console.log(res.json()) // pending됩니다.
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return <div>Product</div>;
+}
