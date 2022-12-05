b98fff21c77099a20a019184dfc82fb92e485611
SeminKang <ksm9802@gmail.com>
2022-12-06 00:54:02 +0900

    221206

---
+++ React2/react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import "./expenseItem.css"
+import Card from '../UI/Card'
+import ExpenseDate from "./ExpenseDate"
+
+export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("updated!");
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+        <ExpenseDate date={props.date}/>
+        <div className='expense-item__description'>
+            <h2>{title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  )
+}
