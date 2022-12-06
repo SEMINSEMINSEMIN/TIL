09c89d598ac3c49988a028bdba4d64e5e4de74c1
강세민 <ksm9802@gmail.com>
2022-12-06 18:13:37 +0900

    221206

--- React2/react-complete-guide/src/components/Expenses/ExpenseItem.jsx
+++ React2/react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react'
-import "./expenseItem.css"
-import Card from '../UI/Card'
-import ExpenseDate from "./ExpenseDate"
+import React from "react";
+import "./expenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("updated!");
-    console.log(title);
-  };
-
-  return (
-    <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
-        <div className='expense-item__description'>
-            <h2>{title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-        </div>
-        <button onClick={clickHandler}>Change Title</button>
-    </Card>
-  )
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
+    );
 }
--- React2/react-complete-guide/src/components/Expenses/Expenses.jsx
+++ React2/react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import "./expenses.css"
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
-export default function Expenses({data}) {
-  return (
-    <Card className='expenses'>
-      <ExpenseItem 
-        title={data[0].title}
-        amount={data[0].amount}
-        date={data[0].date}
-      />
-      <ExpenseItem
-        title={data[1].title}
-        amount={data[1].amount}
-        date={data[1].date}
-      />
-      <ExpenseItem
-        title={data[2].title}
-        amount={data[2].amount}
-        date={data[2].date}
-      />
-      <ExpenseItem
-        title={data[3].title}
-        amount={data[3].amount}
-        date={data[3].date}
-      />  
-    </Card>
-  )
+import React, { useState } from "react";
+import "./expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpenseFilter";
+import Card from "../UI/Card";
+export default function Expenses({ items }) {
+    const [filteredYear, setFilteredYear] = useState("2020");
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = items.filter((e) => {
+        return e.date.getFullYear().toString() === filteredYear;
+    });
+
+    const expenses = filteredExpenses.map((e) => (
+        <ExpenseItem
+            key={e.id}
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+        />
+    ));
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = expenses;
+    }
+
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                {expensesContent}
+            </Card>
+        </div>
+    );
 }
